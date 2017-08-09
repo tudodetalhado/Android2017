95c2f4213f45c2ac3add00a4cf7037113d366e3f
the dark side <geucimar@gmail.com>
2017-08-08 22:00:51 -0300

    Alteração para tocar música e alterar capa.

--- 3BA02_PlayerMP31TAO/app/src/main/java/br/edu/up/a3ba02_playermp3tao/MainActivity.java
+++ 3BA02_PlayerMP31TAO/app/src/main/java/br/edu/up/a3ba02_playermp3tao/MainActivity.java
@@ -1,7 +1,9 @@
 package br.edu.up.a3ba02_playermp3tao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void tocar(View view){
+
+        Intent intent = new Intent(this, PlayerActivity.class);
+        startActivity(intent);
+
+    }
+
 }
--- 3BA02_PlayerMP31TAO/app/src/main/java/br/edu/up/a3ba02_playermp3tao/PlayerActivity.java
+++ 3BA02_PlayerMP31TAO/app/src/main/java/br/edu/up/a3ba02_playermp3tao/PlayerActivity.java
@@ -3,6 +3,8 @@ package br.edu.up.a3ba02_playermp3tao;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -10,9 +12,42 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+    }
 
-        MediaPlayer player = MediaPlayer.create(this, R.raw.sambo_sunday_bloody_sunday);
-        player.start();
+    int[] MP3s = {
+            R.raw.anitta_bang,
+            R.raw.jota_quest_na_moral,
+            R.raw.pink_floyd_another_brick_in_the_wall_p2,
+            R.raw.sambo_sunday_bloody_sunday,
+            R.raw.talking_heads_psycho_killer,
+            R.raw.the_doors_light_my_fire,
+            R.raw.zeca_baleiro_disritmia
+    };
+
+    int[] capas = {
+        R.drawable.anitta
+    };
+
+    MediaPlayer player;
+
+    public void tocarOuPausar(View view){
+
+        if (player == null) {
+            ImageView capa = (ImageView) findViewById(R.id.imgCapa);
+            capa.setImageResource(capas[0]);
+
+            player = MediaPlayer.create(this, MP3s[0]);
+            player.start();
+        }
+    }
+
+    public void parar(View view){
+
+        if (player != null){
+            player.stop();
+            player.release(); //limpar memória;
+            player = null;
+        }
 
     }
 }
