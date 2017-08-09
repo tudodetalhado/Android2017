64fee877b92669e63b91cedbd16e27d5570ac0fc
the dark side <geucimar@gmail.com>
2017-08-08 22:00:51 -0300

    Versão inicial.

--- 3BA02_PlayerMP31TAO/app/src/main/java/br/edu/up/a3ba02_playermp3tao/PlayerActivity.java
+++ 3BA02_PlayerMP31TAO/app/src/main/java/br/edu/up/a3ba02_playermp3tao/PlayerActivity.java
@@ -3,6 +3,9 @@ package br.edu.up.a3ba02_playermp3tao;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -10,9 +13,87 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+    }
 
-        MediaPlayer player = MediaPlayer.create(this, R.raw.sambo_sunday_bloody_sunday);
-        player.start();
 
+
+    int[] capas = {
+            R.drawable.anitta,
+            R.drawable.jota_quest,
+            R.drawable.pink_floyd,
+            R.drawable.sambo,
+            R.drawable.talking_heads,
+            R.drawable.the_doors,
+            R.drawable.zeca_baleiro
+    };
+
+    int[] MP3s = {
+            R.raw.anitta_bang,
+            R.raw.jota_quest_na_moral,
+            R.raw.pink_floyd_another_brick_in_the_wall_p2,
+            R.raw.sambo_sunday_bloody_sunday,
+            R.raw.talking_heads_psycho_killer,
+            R.raw.the_doors_light_my_fire,
+            R.raw.zeca_baleiro_disritmia
+    };
+
+    String[] musicas = {
+            "Bang",
+            "Na moral",
+            "Another brick in the wall",
+            "Sunday bloody sunday",
+            "Psycho killer",
+            "Light my fire",
+            "Disritimia"
+    };
+
+    MediaPlayer player;
+    int musicaAtual = 2;
+
+    public void tocarOuPausar(View view){
+
+        novaMusica();
+    }
+
+    private void novaMusica() {
+        if (player == null) {
+            ImageView capa = (ImageView) findViewById(R.id.imgCapa);
+            capa.setImageResource(capas[musicaAtual]);
+
+            TextView titulo = (TextView) findViewById(R.id.tituloMusica);
+            titulo.setText(musicas[musicaAtual]);
+
+            player = MediaPlayer.create(this, MP3s[musicaAtual]);
+            player.start();
+        }
+    }
+
+    public void proximaMusica(View view){
+        musicaAtual += musicaAtual;
+        parar();
+        novaMusica();
+    }
+
+
+    public void musicaAnterior(View view){
+        musicaAtual -= musicaAtual;
+        parar();
+        novaMusica();
+    }
+
+
+
+    public void parar(View view){
+
+        parar();
+
+    }
+
+    private void parar() {
+        if (player != null){
+            player.stop();
+            player.release(); //limpar memória;
+            player = null;
+        }
     }
 }
--- 3BA02_PlayerMP31TE/app/src/main/java/br/edu/up/a3ba02_playermp31te/MainActivity.java
+++ 3BA02_PlayerMP31TE/app/src/main/java/br/edu/up/a3ba02_playermp31te/MainActivity.java
@@ -1,9 +1,7 @@
-package br.edu.up.a3ba02_playermp3tao;
+package br.edu.up.a3ba02_playermp31te;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,12 +10,4 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void tocar(View view){
-
-        Intent intent = new Intent(this, PlayerActivity.class);
-        startActivity(intent);
-
-    }
-
 }
