6c5a50b97f8a20b43acc624dc1abcd812afea3f1
the dark side <geucimar@gmail.com>
2017-08-10 20:44:51 -0300

    Versão inicial

---
+++ 3BA02_PlayerMP32TE/app/src/main/java/br/edu/up/a3ba02_playermp32te/PlayerActivity.java
@@ -0,0 +1,13 @@
+package br.edu.up.a3ba02_playermp32te;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
