4cac373a3d1371d50e52aec043779f01f95c13fe
Geucimar <geucimar@gmail.com>
2017-08-10 20:33:06 -0300

    Player MP3 tocando música

--- 3BA02_PlayerMP31TE/app/src/main/java/br/edu/up/a3ba02_playermp31te/PlayerActivity.java
+++ 3BA02_PlayerMP31TE/app/src/main/java/br/edu/up/a3ba02_playermp31te/PlayerActivity.java
@@ -1,7 +1,9 @@
 package br.edu.up.a3ba02_playermp31te;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class PlayerActivity extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class PlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+    }
+
+    MediaPlayer player;
+
+    int[] MP3s = {
+            R.raw.anitta_bang,
+            R.raw.jota_quest_na_moral,
+            R.raw.pink_floyd_another_brick_in_the_wall_p2,
+            R.raw.sambo_sunday_bloody_sunday,
+            R.raw.talking_heads_psycho_killer,
+            R.raw.the_doors_light_my_fire,
+            R.raw.zeca_baleiro_disritmia
+    };
+
+    int musicaAtual = 4;
+
+    public void tocarOuPausar(View view){
+        player = MediaPlayer.create(this, MP3s[musicaAtual]);
+        player.start();
+    }
+
+    public void parar(View view){
+
+        if (player != null && player.isPlaying()) {
+            player.stop();
+            player.release(); //liberar memória
+            player = null;
+        }
     }
 }
