9c9e544f4b6c230581b21a7ffdf9052ecd4c02b3
the dark side <geucimar@gmail.com>
2017-08-22 22:17:23 -0300

    Alteração para atualizar barra de progresso automaticamente.

--- 3BA02_PlayerMP31TAO/app/src/main/java/br/edu/up/a3ba02_playermp3tao/PlayerActivity.java
+++ 3BA02_PlayerMP31TAO/app/src/main/java/br/edu/up/a3ba02_playermp3tao/PlayerActivity.java
@@ -2,22 +2,29 @@ package br.edu.up.a3ba02_playermp3tao;
 
 import android.media.Image;
 import android.media.MediaPlayer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends AppCompatActivity
+    implements View.OnTouchListener, MediaPlayer.OnCompletionListener
+{
 
     ImageView btnPlay;
     ImageView btnSequencia;
     boolean isSequencial = true;
     List<Musica> listaDeMusicas = new ArrayList<>();
+    int duration;
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,8 @@ public class PlayerActivity extends AppCompatActivity {
 
         btnPlay = (ImageView) findViewById(R.id.btnPlay);
         btnSequencia = (ImageView) findViewById(R.id.btnSequencia);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnTouchListener(this);
 
         Musica m1 = new Musica();
         m1.setId(1);
@@ -117,8 +126,17 @@ public class PlayerActivity extends AppCompatActivity {
             capa.setImageResource(musica.getCapa());
             TextView titulo = (TextView) findViewById(R.id.tituloMusica);
             titulo.setText(musica.getNome());
+
             player = MediaPlayer.create(this, musica.getMp3());
+            player.setOnCompletionListener(this);
+
+            duration = player.getDuration();
+            seekBar.setMax(duration);
+
             player.start();
+
+            atualizarBarra();
+
         }
         else if (player.isPlaying()) // se o player está tocando...
         {
@@ -126,10 +144,28 @@ public class PlayerActivity extends AppCompatActivity {
             player.pause();
         }
         else
-            { //está pausado...
+        { //está pausado...
             btnPlay.setImageResource(R.drawable.pause50px);
             player.start();
         }
+    }
+
+    private void atualizarBarra() {
+
+        if (player != null && player.isPlaying()) {
+            int position = player.getCurrentPosition();
+            seekBar.setProgress(position);
+
+            Runnable processo = new Runnable() {
+                @Override
+                public void run() {
+                    atualizarBarra();
+                }
+            };
+
+            new Handler().postDelayed(processo, 1000);
+        }
+
     }
 
     int musicaAtual = 2;
@@ -173,4 +209,19 @@ public class PlayerActivity extends AppCompatActivity {
             player = null;
         }
     }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+
+        int position = seekBar.getProgress();
+        player.seekTo(position);
+
+        return false;
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        proximaMusica(null);
+    }
+
 }
