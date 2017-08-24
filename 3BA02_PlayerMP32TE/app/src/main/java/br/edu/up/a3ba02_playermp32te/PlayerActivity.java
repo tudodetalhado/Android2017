41c863b372c32141ba0b2329b5e05e57eeb772ba
the dark side <geucimar@gmail.com>
2017-08-23 22:01:29 -0300

    Versão com avanço automático da música.

--- 3BA02_PlayerMP32TE/app/src/main/java/br/edu/up/a3ba02_playermp32te/PlayerActivity.java
+++ 3BA02_PlayerMP32TE/app/src/main/java/br/edu/up/a3ba02_playermp32te/PlayerActivity.java
@@ -3,20 +3,27 @@ package br.edu.up.a3ba02_playermp32te;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class PlayerActivity extends AppCompatActivity {
+public class PlayerActivity extends AppCompatActivity
+
+    implements MediaPlayer.OnCompletionListener,
+    View.OnTouchListener
+
+{
 
     ImageView btnPlay;
     ImageView capa;
     TextView titulo;
     List<Musica> listaDeMusicas = new ArrayList<>();
-
+    SeekBar seekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,9 @@ public class PlayerActivity extends AppCompatActivity {
         btnPlay = (ImageView) findViewById(R.id.btnPlay);
         capa = (ImageView) findViewById(R.id.imgCapa);
         titulo = (TextView) findViewById(R.id.tituloMusica);
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnTouchListener(this);
+
 
         Musica m1 = new Musica();
         m1.setId(1);
@@ -119,6 +129,12 @@ public class PlayerActivity extends AppCompatActivity {
             titulo.setText(musica.getNome());
             btnPlay.setImageResource(R.drawable.pause50px);
             player = MediaPlayer.create(this, musica.getMp3());
+
+            int duration = player.getDuration();
+            seekBar.setMax(duration);
+
+            player.setOnCompletionListener(this);
+
             player.start();
         } else if (player.isPlaying()) { //Se o player está tocando.
             btnPlay.setImageResource(R.drawable.play50px);
@@ -138,11 +154,24 @@ public class PlayerActivity extends AppCompatActivity {
     }
 
     public void parar(){
-        if (player != null && player.isPlaying()){
+        if (player != null){
             player.stop();
             player.release(); //limpar memória
             player = null;
         }
     }
 
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+        proximaMusica(null);
+    }
+
+  @Override
+  public boolean onTouch(View view, MotionEvent motionEvent) {
+
+    int position = seekBar.getProgress();
+    player.seekTo(position);
+
+    return false;
+  }
 }
