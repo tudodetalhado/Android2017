e425882423a1e9f4632fbd53a406c021960fee7e
Geucimar <geucimar@gmail.com>
2017-08-30 22:16:21 -0300

    Versão com lista simples.

--- 3BA02_PlayerMP32TE/app/src/main/java/br/edu/up/a3ba02_playermp32te/MainActivity.java
+++ 3BA02_PlayerMP32TE/app/src/main/java/br/edu/up/a3ba02_playermp32te/MainActivity.java
@@ -4,20 +4,58 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  List<Musica> listaDeMusicas = new ArrayList<>();
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    //Carrega as músicas do banco de dados;
+    BancoDeDados banco = new BancoDeDados();
+    listaDeMusicas = banco.getListaDeMusicas();
+
+    listaDeMusicas.get(0);
+
+    //Converte as músicas para um vetor de Strings;
+    String[] nomeDasMusicas = new String[listaDeMusicas.size()];
+    for(int i = 0; i < listaDeMusicas.size(); i++){
+      nomeDasMusicas[i] = listaDeMusicas.get(i).getNome();
     }
 
-    public void tocar(View view){
+    //Criar o adapter para colocar as múscas no listview;
+    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        android.R.layout.simple_list_item_1, nomeDasMusicas);
 
-        Intent intent = new Intent(this, PlayerActivity.class);
+    //Carrega o listview do Xml no Java e define o adapter;
+    ListView listView = (ListView) findViewById(R.id.listViewMusicas);
+    listView.setAdapter(adapter);
+
+    //Vincular o clique do item;
+    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        //Toast.makeText(MainActivity.this, "Item: " + position, Toast.LENGTH_LONG ).show();
+
+        Intent intent = new Intent(MainActivity.this, PlayerActivity.class);
+        intent.putExtra("musPosition", position);
         startActivity(intent);
 
-    }
+      }
+    });
+    //listView.setOnItemLongClickListener();
+    //listView.setOnDragListener();
+  }
+
 
 }
--- 3BA02_PlayerMP32TE/app/src/main/java/br/edu/up/a3ba02_playermp32te/PlayerActivity.java
+++ 3BA02_PlayerMP32TE/app/src/main/java/br/edu/up/a3ba02_playermp32te/PlayerActivity.java
@@ -1,5 +1,6 @@
 package br.edu.up.a3ba02_playermp32te;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
@@ -20,176 +21,125 @@ public class PlayerActivity extends AppCompatActivity
 
 {
 
-    ImageView btnPlay;
-    ImageView capa;
-    TextView titulo;
-    List<Musica> listaDeMusicas = new ArrayList<>();
-    SeekBar seekBar;
+  ImageView btnPlay;
+  ImageView capa;
+  TextView titulo;
+  List<Musica> listaDeMusicas = new ArrayList<>();
+  SeekBar seekBar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_player);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_player);
 
-        btnPlay = (ImageView) findViewById(R.id.btnPlay);
-        capa = (ImageView) findViewById(R.id.imgCapa);
-        titulo = (TextView) findViewById(R.id.tituloMusica);
-        seekBar = (SeekBar) findViewById(R.id.seekBar);
-        seekBar.setOnTouchListener(this);
+    btnPlay = (ImageView) findViewById(R.id.btnPlay);
+    capa = (ImageView) findViewById(R.id.imgCapa);
+    titulo = (TextView) findViewById(R.id.tituloMusica);
+    seekBar = (SeekBar) findViewById(R.id.seekBar);
+    seekBar.setOnTouchListener(this);
+
+    BancoDeDados banco = new BancoDeDados();
+    listaDeMusicas = banco.getListaDeMusicas();
+
+    Intent intent = getIntent();
+    musicaAtual = intent.getIntExtra("musPosition", 0);
+    tocar();
+  }
+  int musicaAtual;
+
+  MediaPlayer player;
 
 
-        Musica m1 = new Musica();
-        m1.setId(1);
-        m1.setArtista("Anitta");
-        m1.setNome("Bang");
-        m1.setCapa(R.drawable.anitta);
-        m1.setMp3(R.raw.anitta_bang);
 
-        Musica m2 = new Musica();
-        m2.setId(2);
-        m2.setArtista("Jota Quest");
-        m2.setNome("Na moral");
-        m2.setCapa(R.drawable.jota_quest);
-        m2.setMp3(R.raw.jota_quest_na_moral);
-
-        Musica m3 = new Musica();
-        m3.setId(3);
-        m3.setArtista("Pink Floyd");
-        m3.setNome("Another Brick in the Wall");
-        m3.setCapa(R.drawable.pink_floyd);
-        m3.setMp3(R.raw.pink_floyd_another_brick_in_the_wall_p2);
-
-        Musica m4 = new Musica();
-        m4.setId(4);
-        m4.setArtista("Sambô");
-        m4.setNome("Sunday bloody sunday");
-        m4.setCapa(R.drawable.sambo);
-        m4.setMp3(R.raw.sambo_sunday_bloody_sunday);
-
-        Musica m5 = new Musica();
-        m5.setId(5);
-        m5.setArtista("Talking Heads");
-        m5.setNome("Psycho Killer");
-        m5.setCapa(R.drawable.talking_heads);
-        m5.setMp3(R.raw.talking_heads_psycho_killer);
-
-        Musica m6 = new Musica();
-        m6.setId(6);
-        m6.setArtista("The Doors");
-        m6.setNome("Light my fire");
-        m6.setCapa(R.drawable.the_doors);
-        m6.setMp3(R.raw.the_doors_light_my_fire);
-
-        Musica m7 = new Musica();
-        m7.setId(7);
-        m7.setArtista("Zeca Baleiro");
-        m7.setNome("Disritimia");
-        m7.setCapa(R.drawable.zeca_baleiro);
-        m7.setMp3(R.raw.zeca_baleiro_disritmia);
-
-        listaDeMusicas.add(m1);
-        listaDeMusicas.add(m2);
-        listaDeMusicas.add(m3);
-        listaDeMusicas.add(m4);
-        listaDeMusicas.add(m5);
-        listaDeMusicas.add(m6);
-        listaDeMusicas.add(m7);
-
+  public void proximaMusica(View view) {
+    musicaAtual = musicaAtual + 1;
+    if (musicaAtual > listaDeMusicas.size() - 1) {
+      musicaAtual = 0;
     }
 
-    MediaPlayer player;
+    parar();
+    tocar();
+  }
 
-    int musicaAtual = 3;
+  public void musicaAnterior(View view) {
+    musicaAtual = musicaAtual - 1;
 
-    public void proximaMusica(View view){
-        musicaAtual = musicaAtual + 1;
-        if (musicaAtual > listaDeMusicas.size() -1){
-            musicaAtual = 0;
+    if (musicaAtual < 0) {
+      musicaAtual = listaDeMusicas.size() - 1;
+    }
+
+    parar();
+    tocar();
+  }
+
+  public void tocar() {
+    if (player == null) { //Se o player não existe
+
+      Musica musica = listaDeMusicas.get(musicaAtual);
+      capa.setImageResource(musica.getCapa());
+      titulo.setText(musica.getNome());
+      btnPlay.setImageResource(R.drawable.pause50px);
+      player = MediaPlayer.create(this, musica.getMp3());
+
+      int duration = player.getDuration();
+      seekBar.setMax(duration);
+
+      player.setOnCompletionListener(this);
+
+      player.start();
+
+      atualizarBarra();
+
+
+    } else if (player.isPlaying()) { //Se o player está tocando.
+      btnPlay.setImageResource(R.drawable.play50px);
+      player.pause();
+    } else { //volta a tocar
+      btnPlay.setImageResource(R.drawable.pause50px);
+      player.start();
+    }
+  }
+
+  private void atualizarBarra() {
+
+    if (player != null && player.isPlaying()) {
+
+      int position = player.getCurrentPosition();
+      seekBar.setProgress(position);
+
+      Runnable processo = new Runnable() {
+        @Override
+        public void run() {
+          atualizarBarra();
         }
+      };
 
-        parar();
-        tocar();
-    }
-
-    public void musicaAnterior(View view){
-        musicaAtual = musicaAtual - 1;
-
-        if (musicaAtual < 0){
-            musicaAtual = listaDeMusicas.size() -1;
-        }
-
-        parar();
-        tocar();
-    }
-
-    public void tocar(){
-        if (player == null) { //Se o player não existe
-
-            Musica musica = listaDeMusicas.get(musicaAtual);
-            capa.setImageResource(musica.getCapa());
-            titulo.setText(musica.getNome());
-            btnPlay.setImageResource(R.drawable.pause50px);
-            player = MediaPlayer.create(this, musica.getMp3());
-
-            int duration = player.getDuration();
-            seekBar.setMax(duration);
-
-            player.setOnCompletionListener(this);
-
-            player.start();
-
-            atualizarBarra();
-
-
-        } else if (player.isPlaying()) { //Se o player está tocando.
-            btnPlay.setImageResource(R.drawable.play50px);
-            player.pause();
-        } else { //volta a tocar
-            btnPlay.setImageResource(R.drawable.pause50px);
-            player.start();
-        }
-    }
-
-    private void atualizarBarra() {
-
-      if (player.isPlaying()) {
-
-        int position = player.getCurrentPosition();
-        seekBar.setProgress(position);
-
-        Runnable processo = new Runnable() {
-          @Override
-          public void run() {
-            atualizarBarra();
-          }
-        };
-
-        new Handler().postDelayed(processo, 1000);
-
-      }
+      new Handler().postDelayed(processo, 1000);
 
     }
 
-    public void tocarOuPausar(View view){
-        tocar();
-    }
+  }
 
-    public void parar(View view){
-        parar();
-    }
+  public void tocarOuPausar(View view) {
+    tocar();
+  }
 
-    public void parar(){
-        if (player != null){
-            player.stop();
-            player.release(); //limpar memória
-            player = null;
-        }
-    }
+  public void parar(View view) {
+    parar();
+  }
 
-    @Override
-    public void onCompletion(MediaPlayer mediaPlayer) {
-        proximaMusica(null);
+  public void parar() {
+    if (player != null) {
+      player.stop();
+      player.release(); //limpar memória
+      player = null;
     }
+  }
+
+  @Override
+  public void onCompletion(MediaPlayer mediaPlayer) {
+    proximaMusica(null);
+  }
 
   @Override
   public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -198,5 +148,11 @@ public class PlayerActivity extends AppCompatActivity
     player.seekTo(position);
 
     return false;
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    parar();
   }
 }
